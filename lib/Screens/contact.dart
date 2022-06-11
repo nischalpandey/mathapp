65b0f7a9b0134873886d42ccd7ffc6e7d400f30a
Nischal Pandey <admin@nischalpandey.com.np>
2022-06-11 16:47:22 +0545

    level1

--- lib/Screens/contact.dart
+++ lib/Screens/contact.dart
@@ -14,7 +14,7 @@ class _ContactState extends State<Contact> {
       child: Column(
         children: [
           Text("Contact"),
-          Center(child: Image.asset('asset/img/contact.png'))
+          Center(child: Image.asset('assets/img/contact.png'))
         ],
       ),
     );
